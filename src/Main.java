345b9ecb93a489a5ffad7eb8c332043850065e87
Kirill <c.jdanow2017@yandex.ru>
2020-05-18 21:41:52 +0700

    HW 1

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,14 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Main {
     public static void main(String[] args) {
-        for (int i = 0; i < 10; i++) {
-            System.out.println("Denis musor");
-        }
+        double a = 450.242;
+        double b = 44.32;
+        double c = a * b;
+        System.out.println(c);
     }
 }
+
+   
